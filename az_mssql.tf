1e976bf6c89885f16384b10363887ab27c34277e
Taylor <28880387+tsmithv11@users.noreply.github.com>
2022-01-19 20:28:23 -0800

    Update az_mssql.tf

--- az_mssql.tf
+++ az_mssql.tf
@@ -131,3 +131,54 @@ resource "azurerm_storage_account" "sa5" {
   account_replication_type = "LRS"
   enable_https_traffic_only = true
 }
+
+resource "azurerm_mssql_server" "mssql6" {
+  name                         = "mssql6"
+  resource_group_name          = azurerm_resource_group.az_rg.name
+  location                     = azurerm_resource_group.az_rg.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.sa6.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.sa6.primary_access_key
+    storage_account_access_key_is_secondary = false
+    retention_in_days                       = 180
+  }
+}
+
+resource "azurerm_storage_account" "sa6" {
+  name                     = "sa6"
+  resource_group_name      = azurerm_resource_group.az_rg.name
+  location                 = azurerm_resource_group.az_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
+}
+
+resource "azurerm_mssql_server" "mssql7" {
+  name                         = "mssql7"
+  resource_group_name          = azurerm_resource_group.az_rg.name
+  location                     = azurerm_resource_group.az_rg.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.sa7.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.sa7.primary_access_key
+    storage_account_access_key_is_secondary = false
+    retention_in_days                       = 180
+  }
+}
+
+resource "azurerm_storage_account" "sa7" {
+  name                     = "sa7"
+  resource_group_name      = azurerm_resource_group.az_rg.name
+  location                 = azurerm_resource_group.az_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
+}
+
