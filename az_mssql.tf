9edf8fa2abdc58c89db34f28a3917590e98748c3
Taylor <28880387+tsmithv11@users.noreply.github.com>
2022-01-19 21:24:22 -0800

    Update az_mssql.tf

--- az_mssql.tf
+++ az_mssql.tf
@@ -4,7 +4,16 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "az_rg" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "West US"
+}
+
+resource "azurerm_storage_account" "security_storage_account" {
+  name                     = "securitystorageaccount"
+  resource_group_name      = azurerm_resource_group.az_rg.name
+  location                 = azurerm_resource_group.az_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server" "mssql1" {
@@ -14,22 +23,6 @@ resource "azurerm_mssql_server" "mssql1" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
-
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.sa1.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.sa1.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 180
-  }
-}
-
-resource "azurerm_storage_account" "sa1" {
-  name                     = "sa1"
-  resource_group_name      = azurerm_resource_group.az_rg.name
-  location                 = azurerm_resource_group.az_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server" "mssql2" {
@@ -39,22 +32,6 @@ resource "azurerm_mssql_server" "mssql2" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
-
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.sa2.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.sa2.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 180
-  }
-}
-
-resource "azurerm_storage_account" "sa2" {
-  name                     = "sa2"
-  resource_group_name      = azurerm_resource_group.az_rg.name
-  location                 = azurerm_resource_group.az_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server" "mssql3" {
@@ -64,22 +41,6 @@ resource "azurerm_mssql_server" "mssql3" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
-
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.sa3.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.sa3.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 180
-  }
-}
-
-resource "azurerm_storage_account" "sa3" {
-  name                     = "sa3"
-  resource_group_name      = azurerm_resource_group.az_rg.name
-  location                 = azurerm_resource_group.az_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server" "mssql4" {
@@ -89,22 +50,6 @@ resource "azurerm_mssql_server" "mssql4" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
-
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.sa4.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.sa4.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 180
-  }
-}
-
-resource "azurerm_storage_account" "sa4" {
-  name                     = "sa4"
-  resource_group_name      = azurerm_resource_group.az_rg.name
-  location                 = azurerm_resource_group.az_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server" "mssql5" {
@@ -114,22 +59,6 @@ resource "azurerm_mssql_server" "mssql5" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
-
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.sa5.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.sa5.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 7
-  }
-}
-
-resource "azurerm_storage_account" "sa5" {
-  name                     = "sa5"
-  resource_group_name      = azurerm_resource_group.az_rg.name
-  location                 = azurerm_resource_group.az_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server" "mssql6" {
@@ -139,22 +68,6 @@ resource "azurerm_mssql_server" "mssql6" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
-
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.sa6.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.sa6.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 180
-  }
-}
-
-resource "azurerm_storage_account" "sa6" {
-  name                     = "sa6"
-  resource_group_name      = azurerm_resource_group.az_rg.name
-  location                 = azurerm_resource_group.az_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server" "mssql7" {
@@ -164,22 +77,6 @@ resource "azurerm_mssql_server" "mssql7" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
-
-  extended_auditing_policy {
-    storage_endpoint                        = azurerm_storage_account.sa7.primary_blob_endpoint
-    storage_account_access_key              = azurerm_storage_account.sa7.primary_access_key
-    storage_account_access_key_is_secondary = false
-    retention_in_days                       = 180
-  }
-}
-
-resource "azurerm_storage_account" "sa7" {
-  name                     = "sa7"
-  resource_group_name      = azurerm_resource_group.az_rg.name
-  location                 = azurerm_resource_group.az_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
 }
 
 resource "azurerm_mssql_server_security_alert_policy" "alertpolicy1" {
@@ -188,8 +85,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy1" {
   resource_group_name        = azurerm_resource_group.az_rg.name
   server_name                = azurerm_mssql_server.mssql1.name
   state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.sa1.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.sa1.primary_access_key
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
   disabled_alerts = [
     "Sql_Injection",
     "Data_Exfiltration"
@@ -204,8 +101,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy2" {
   resource_group_name        = azurerm_resource_group.az_rg.name
   server_name                = azurerm_mssql_server.mssql2.name
   state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.sa2.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.sa2.primary_access_key
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
   disabled_alerts = [
     "Sql_Injection",
     "Data_Exfiltration"
@@ -220,8 +117,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy3" {
   resource_group_name        = azurerm_resource_group.az_rg.name
   server_name                = azurerm_mssql_server.mssql3.name
   state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.sa3.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.sa3.primary_access_key
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
   disabled_alerts = [
     "Sql_Injection",
     "Data_Exfiltration"
@@ -236,8 +133,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy4" {
   resource_group_name        = azurerm_resource_group.az_rg.name
   server_name                = azurerm_mssql_server.mssql4.name
   state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.sa4.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.sa4.primary_access_key
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
   disabled_alerts = [
     "Sql_Injection",
     "Data_Exfiltration"
@@ -252,8 +149,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy5" {
   resource_group_name        = azurerm_resource_group.az_rg.name
   server_name                = azurerm_mssql_server.mssql5.name
   state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.sa5.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.sa5.primary_access_key
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
   disabled_alerts = [
     "Sql_Injection",
     "Data_Exfiltration"
@@ -267,8 +164,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy6" {
   resource_group_name        = azurerm_resource_group.az_rg.name
   server_name                = azurerm_mssql_server.mssql6.name
   state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.sa6.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.sa6.primary_access_key
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
   disabled_alerts = [
     "Sql_Injection",
     "Data_Exfiltration"
@@ -283,8 +180,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy7" {
   resource_group_name        = azurerm_resource_group.az_rg.name
   server_name                = azurerm_mssql_server.mssql7.name
   state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.sa7.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.sa7.primary_access_key
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
   disabled_alerts = [
     "Sql_Injection",
     "Data_Exfiltration"
